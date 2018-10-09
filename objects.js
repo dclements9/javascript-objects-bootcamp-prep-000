1c549f79575f8594a6bd26019308f426e46fc4be
dclements <dclements@protonmail.com>
2018-10-09 16:34:38 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,3 +8,7 @@ function updatePlaylist(object,artistName,songTitle){
   playlist [artistName] = songTitle
     return playlist
 }
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName
+  return playlist
+}
