021a3bbb3460148353cb521b35ae7a35d8c46b74
dclements <dclements@protonmail.com>
2018-10-09 16:53:31 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,8 @@
 var playlist = {
-  "John Lennon": "Imagine",
-  "Von Wegen Lisbeth": "Meine Kneipe",
-  "Jack Johnson": "Radiate",
-  "Nathaniel Rateliff": "I Need Never get Old"
+  ["John Lennon"]: "Imagine",
+  ["Von Wegen Lisbeth"]: "Meine Kneipe",
+  ["Jack Johnson"]: "Radiate",
+  ["Nathaniel Rateliff"]: "I Need Never get Old"
 }
 function updatePlaylist(object,artistName,songTitle){
   playlist [artistName] = songTitle
@@ -11,4 +11,5 @@ function updatePlaylist(object,artistName,songTitle){
 function removeFromPlaylist(playlist, artistName){
   delete playlist.artistName
   return playlist
+  //return Object.assign({}, playlist)
 }
