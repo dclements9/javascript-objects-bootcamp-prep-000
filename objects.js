9f0075d606ab85e8bdc991709dfb987b142a827c
dclements <dclements@protonmail.com>
2018-10-09 16:18:39 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var Playlist = {
+  John Lennon: "Imagine",
+  Von Wegen Lisbeth: "Meine Kneipe",
+  Jack Johnson: "Radiate",
+  Nathaniel Rateliff: "I Need Never get Old"
+}
+
+function updatePlaylist(object,artistName,songTitle){
+  Playlist[ 'artistName' = "songTitle"]
+    return Playlist
+}
